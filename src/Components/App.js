9192914375b7233e0c659a7acb29f59177b43304
Nisarg Patel <nisarg38@gmail.com>
2021-08-09 11:25:23 +0530

    Update

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,9 @@
 import './App.css';
 import {React} from 'react';
-import git from "images/git.png";
-import repo from "images/repo.gif";
 import Search from "./Search"
 
 function App() {
 
-
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=ahmedabad&appid=d8be39510f66b94d1859317a8e9cab91')
-  .then(res => res.json())
-  .then((data) => {
-      this.setState({ contacts: data })
-  })
-  .catch(console.log)
 
   return (
 
@@ -23,17 +14,18 @@ function App() {
         <h1 className="title">WeatherApp</h1>
           
           <a href="https://github.com/Nisarg38" target="_blank" rel="noopener noreferrer" className="git" >
-            <img alt="github" src={git}
+            <img alt="github" src="https://raw.githubusercontent.com/Nisarg38/Sorting-Algo-Visualizer-Js/main/images/GitHub-Mark-Light-120px-plus.png"
             width="50" height="50"/>
           </a>
           <a href="https://github.com/Nisarg38" target="_blank" rel="noopener noreferrer" className="repo">
-            <img alt="github" src={repo}
+            <img alt="github" src="https://raw.githubusercontent.com/Nisarg38/Sorting-Algo-Visualizer-Js/main/images/1331-repository-outline%20(1).gif"
             width="100" height="100"/>
           </a>
         </div>
 
         <Search
         />
+
     
 
     </div>
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,34 @@
-import {React} from 'react';
+import {React,useEffect, useState } from 'react';
+import Weather from './weather';
 
 function Search() {
 
 
-   
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
+
+
+
+    
+   
   return (
 
     <div className="main" >
@@ -14,6 +38,8 @@ function Search() {
             <input type="submit" value="Submit" />
           </form>
         
+   
+
     </div>
 
   );
