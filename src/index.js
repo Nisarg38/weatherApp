bce2e24bdb9d74e281240e80b3f735b649636929
Nisarg Patel <nisarg38@gmail.com>
2021-08-09 22:32:59 +0530

    2.0

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/App';
-import 'semantic-ui-css/semantic.min.css'
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
